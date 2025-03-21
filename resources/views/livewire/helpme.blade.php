e1f81b9445f94f5eff32df469392dd660c226ab0
only1blank <omnomyoshi@gmail.com>
2025-03-21 14:57:42 +0600

    first commit

---
+++ resources/views/livewire/helpme.blade.php
@@ -0,0 +1,17 @@
+<div>
+<div class="help-form">
+    <div class="help-form__row">
+        <div class="help-form__row_guywithnotebook">
+<img src="{{asset('img/help.png')}}" alt="">
+        </div>
+        <div class="help-form__row_form">
+<form action="" method="post">
+    <label class="help-form_form_question" for="">Появились вопросы?</label>
+    <p class="help-form_form_description">Техническая поддержка всегда готова ответить на все интересующие вас вопросы</p>
+    <button class="help-form_form_btn">Написать</button>
+</form>
+
+        </div>
+    </div>
+</div>
+</div>
